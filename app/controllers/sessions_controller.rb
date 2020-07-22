e92c83570517f093c791a8cde5709ede68648453
jmknopf1007 <jmknopf1007@gmail.com>
2020-07-22 16:00:59 -0700

    updated flash message in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to books_path 
         else 
-            flash[:message] = "User not found, please try again."
+            flash[:message] = "User not found, please try again!" 
             render :login
         end
     end
