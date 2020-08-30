3ceec7a0536acbaabd85638f0376d46253691cba
jmknopf1007 <jmknopf1007@gmail.com>
2020-08-29 23:34:59 -0700

    updated user model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :songs, through: :reviews
+    has_many :books, through: :reviews
 
     validates :username, presence: true
     validates :username, uniqueness: true 
